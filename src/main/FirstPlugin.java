8fadb16ec44af187e57aac4086453cc2beaffaf4
angelless <dongkyu91622@gmail.com>
2016-10-04 14:09:41 +0900

    #1

--- src/main/FirstPlugin.java
+++ src/main/FirstPlugin.java
@@ -1,2 +1,17 @@
 package main;
 
+import cn.nukkit.event.Listener;
+import cn.nukkit.plugin.PluginBase;
+
+class FirstPlugin extends PluginBase implements Listener{
+	@Override
+	public void onEnable(){
+		this.getServer().getPluginManager().registerEvents(this, this);
+		this.getLogger().info("FirstPlugin 활성화");
+	}
+	@Override
+	public void onDisable(){
+		this.getLogger().info("FirstPlugin 비활성화");
+		super.onDisable();
+	}
+}
