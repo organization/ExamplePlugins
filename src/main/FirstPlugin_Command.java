ca5cea7600b3df2010c0d9c96c276510626636f7
OverTook <OverTook@users.noreply.github.com>
2016-10-10 20:56:45 +0900

    Update FirstPlugin_Command.java

--- src/main/FirstPlugin_Command.java
+++ src/main/FirstPlugin_Command.java
@@ -1,1 +1,24 @@
 package main;
+
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.plugin.PluginBase;
+
+public class FirstPlugin_Command extends PluginBase{
+  
+  @Override
+  public void onEnable(){
+    this.getLogger().info("FirstPlugin_Command Loaded");
+  }
+  
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+    try{
+      Position spawn = ((Player) sender).teleport(((Player) sender).getSpawn());
+      return true;
+    } catch(ClassCastException e){
+      //이러한 경우에는 콘솔에서 친거인지 확인하세요.
+      return false;
+    }
+    return true;
+    
