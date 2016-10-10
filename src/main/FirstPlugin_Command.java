5bae63e90d880ab5e3c6dc579bff8621a0eb9903
OverTook <OverTook@users.noreply.github.com>
2016-10-10 20:57:59 +0900

    Update FirstPlugin_Command.java

--- src/main/FirstPlugin_Command.java
+++ src/main/FirstPlugin_Command.java
@@ -4,6 +4,7 @@ import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.Player;
+import cn.nukkit.level.Position;
 
 public class FirstPlugin_Command extends PluginBase{
   
